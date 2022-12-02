4cf503cfb72d6b5ff6d97806bdeca121964585a3
n800s <n800sau@gmail.com>
2022-12-02 11:25:42 +1100

    test_dds adjusted

--- test_dds/node_list.sh
+++ test_dds/node_list.sh
@@ -2,3 +2,4 @@ export ROS_DISCOVERY_SERVER=192.168.1.50:11811
 export ROS_DOMAIN_ID=0
 export FASTRTPS_DEFAULT_PROFILES_FILE=fastdds_config_auxiliar_server.xml
 ros2 node list --no-daemon
+#ros2 node info /nav2_container --no-daemon
