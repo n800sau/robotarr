6900c9141ec068c26786ab45ffc0c6fa5f40f46f
Unknown <n800sau@gmail.com>
2022-12-06 22:58:34 +0000

    fixed sourcing for symlink

--- 3wheeler/ros1_ws/scripts_extern/run.sh
+++ 3wheeler/ros1_ws/scripts_extern/run.sh
@@ -1,6 +1,7 @@
 #. /opt/ros/noetic/setup.bash
 export ROS_MASTER_URI=http://gate:11311
-. ../devel/setup.bash
+DIR="`dirname $PWD`"
+. `realpath $DIR`/devel/setup.bash
 echo $ROS_MASTER_URI
 roslaunch main.launch &>run.log
 
