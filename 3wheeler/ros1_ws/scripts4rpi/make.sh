d6fa6f0b7192a992fd6a03c79e83ed9fd9e27b3c
Unknown <n800sau@gmail.com>
2022-12-14 15:41:44 +1100

    make install and wheeler3_rpc as main script

--- 3wheeler/ros1_ws/scripts4rpi/make.sh
+++ 3wheeler/ros1_ws/scripts4rpi/make.sh
@@ -2,5 +2,5 @@
 LOGPATH="`pwd`/make.log"
 . /opt/ros/noetic/setup.bash
 cd ..
-catkin_make -j1 &>"${LOGPATH}"
+catkin_make -j1 install &>"${LOGPATH}"
 echo $?
--- 3wheeler/ros1_ws/scripts4rpi/run.sh
+++ 3wheeler/ros1_ws/scripts4rpi/run.sh
@@ -1,5 +1,5 @@
 export ROS_MASTER_URI="http://rpi2:11311"
 DIR="`dirname $PWD`"
-. `realpath $DIR`/devel/setup.bash
+. `realpath $DIR`/install/setup.bash
 echo $ROS_MASTER_URI
 roslaunch main.launch &>run.log
