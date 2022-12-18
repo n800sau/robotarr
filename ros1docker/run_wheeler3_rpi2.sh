a4d95c6252def9bc668f5f8ce40af96b310fb0b6
Unknown <n800sau@gmail.com>
2022-12-19 09:43:55 +1100

    added restarting option

--- ros1docker/run_wheeler3_rpi2.sh
+++ ros1docker/run_wheeler3_rpi2.sh
@@ -2,5 +2,5 @@
 
 docker run --name wheeler3 --network host --mount type=bind,source=/home/n800s,target=/home/n800s -v /dev:/dev --privileged \
 	--add-host rpi2:127.0.0.1 --add-host gate:192.168.1.50 --add-host big:192.168.1.5 --restart unless-stopped \
-	-it ros1wheeler3_1
+	ros1wheeler3
 
